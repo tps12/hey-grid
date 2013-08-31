07d37ccc1beacdc4f64d0e4762fe27cbc9ce5c0e
tps12 <tps12@columbia.edu>
2013-08-31 18:28:51 -0400

    Draw pentagons oriented in the least awkward way

--- griddetail.py
+++ griddetail.py
@@ -7,7 +7,8 @@ N, NW, SW, S, SE, NE = range(6)
 dirs = ['N', 'NW', 'SW', 'S', 'SE', 'NE']
 vs = [(-1, sqrt(3)), (1, sqrt(3)), (2, 0), (1, -sqrt(3)), (-1, -sqrt(3)), (-2, 0)]
 offsets = [(0, 2*sqrt(3)), (-3, sqrt(3)), (-3, -sqrt(3)), (0, -2*sqrt(3)), (3, -sqrt(3)), (3, sqrt(3))]
-proto = QPolygonF([QPointF(*cs) for cs in vs])
+hexproto = QPolygonF([QPointF(*cs) for cs in vs])
+pentproto = QPolygonF([QPointF(*cs) for cs in [(0, sqrt(3))] + vs[2:]])
 
 class GridDetail(QGraphicsScene):
     def __init__(self, grids, colors, face):
@@ -36,15 +37,18 @@ class GridDetail(QGraphicsScene):
         # initialize queue with face and arbitrarily chosen local North edge:
         # queue items are (face, direction traversed from, edge crossed, offset) tuples
         q = [(face, S, sorted(grid.faces[face][0:2]), (0,0))]
+        pents = []
         seen = set()
         while len(q) > 0:
             face, whence, edge, offset = q.pop()
             if face not in seen:
+                seen.add(face)
                 vertices = grid.faces[face]
                 if len(vertices) == 5:
+                    pents.append((face, offset))
                     continue
                 color = QColor(*[s * 255 for s in colors[face]])
-                self.addPolygon(proto.translated(*offset), QPen(Qt.transparent), color)
+                self.addPolygon(hexproto.translated(*offset), QPen(Qt.transparent), color)
                 edges = [sorted(vs) for vs in zip(vertices, vertices[1:] + vertices[0:1])]
                 # edges are in CCW order: find edge of origin in list to orient
                 source = edges.index(edge)
@@ -57,5 +61,17 @@ class GridDetail(QGraphicsScene):
                         nextoffset = tuple([offset[i] + offsets[nextdir][i] for i in range(2)])
                         q.insert(0, (commonfaces[0], (nextdir + 3) % 6, border, nextoffset))
                     count += 1
-                seen.add(face)
+        for face, offset in pents:
+            populated = []
+            for ni in range(len(offsets)):
+                if self.itemAt(*[offset[i] + offsets[ni][i] for i in range(2)]) is not None:
+                    if len(populated) > 0 and populated[-1] + 1 != ni:
+                        ni -= 6
+                    populated.append(ni)
+            base = sorted(populated)[len(populated)/2] if len(populated) > 0 else 0
+
+            color = QColor(*[s * 255 for s in colors[face]])
+            polygon = self.addPolygon(pentproto.translated(*offset), QPen(Qt.transparent), color)
+            polygon.setTransformOriginPoint(*offset)
+            polygon.setRotation(60 * (base + 3))
         self.update()
