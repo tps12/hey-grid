93aa7edb4e0992992bd261c0fbc2df9d18738fc4
tps12 <tps12@columbia.edu>
2013-09-13 19:16:54 -0400

    Use FreeMono 14 font

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from sys import argv, exit
-from PySide.QtGui import QApplication, QFont, QFontMetrics
+from PySide.QtGui import QApplication, QFont, QFontDatabase, QFontMetrics
 from mainwindow import MainWindow
 
 app = QApplication(argv)
-font = QFont(QApplication.font())
-font.setPointSize(9)
+font = QFont(QFontDatabase.applicationFontFamilies(QFontDatabase.addApplicationFont('FreeMono.otf'))[0])
+font.setPointSize(14)
 QApplication.setFont(font)
 w = MainWindow()
 metrics = QFontMetrics(font)
