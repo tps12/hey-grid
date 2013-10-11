e40b95c40972ddba2e4a88f0817d2ee9db4fa4c7
tps12 <tps12@columbia.edu>
2013-10-11 08:47:20 -0400

    Rename file

--- screenpresenter.py
+++ screenpresenter.py
@@ -8,7 +8,7 @@ from PySide.QtGui import QFont, QKeyEvent, QWidget, QWidgetItem
 
 from grid import dot, normal, Grid
 from griddetail import GridDetail
-from hellogl import SphereView
+from sphereview import SphereView
 
 from colors import earth, gray, rgb, simplex
 
