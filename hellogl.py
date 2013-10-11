89c5be3d8d4cf7c970c7b7fb9743393e2638a058
tps12 <tps12@columbia.edu>
2013-10-11 08:04:30 -0400

    Remove unused demo code

--- hellogl.py
+++ hellogl.py
@@ -2,7 +2,6 @@
 
 """PySide port of the opengl/hellogl example from Qt v4.x"""
 
-import sys
 import math
 from OpenGL import GL
 from PySide import QtCore, QtOpenGL
@@ -30,11 +29,6 @@ class GLWidget(QtOpenGL.QGLWidget):
 		self.yRot = self._rotationoffset * 16
 		self.zRot = 0
 
-		self.lastPos = QtCore.QPoint()
-
-		self.trolltechGreen = QtGui.QColor.fromCmykF(0.40, 0.0, 1.0, 0.0)
-		self.trolltechPurple = QtGui.QColor.fromCmykF(0.39, 0.39, 0.0, 0.0)
-
 	def minimumSizeHint(self):
 		return QtCore.QSize(50, 50)
 
@@ -53,7 +47,6 @@ class GLWidget(QtOpenGL.QGLWidget):
 			self.updateGL()
 
 	def initializeGL(self):
-		self.qglClearColor(self.trolltechPurple.darker())
 		self.objects = [None for _ in range(self.grid.size + 1)]
 		self.update()
 		GL.glShadeModel(GL.GL_SMOOTH)
@@ -116,81 +109,6 @@ class GLWidget(QtOpenGL.QGLWidget):
 		GL.glEndList()
 
 		return genList
-
-	def makeObject(self):
-		genList = GL.glGenLists(1)
-		GL.glNewList(genList, GL.GL_COMPILE)
-
-		GL.glBegin(GL.GL_QUADS)
-
-		# "T" and tail of "Q"
-		x1 = +0.06
-		y1 = -0.14
-		x2 = +0.14
-		y2 = -0.06
-		x3 = +0.08
-		y3 = +0.00
-		x4 = +0.30
-		y4 = +0.22
-
-		self.quad(x1, y1, x2, y2, y2, x2, y1, x1)
-		self.quad(x3, y3, x4, y4, y4, x4, y3, x3)
-
-		self.extrude(x1, y1, x2, y2)
-		self.extrude(x2, y2, y2, x2)
-		self.extrude(y2, x2, y1, x1)
-		self.extrude(y1, x1, x1, y1)
-		self.extrude(x3, y3, x4, y4)
-		self.extrude(x4, y4, y4, x4)
-		self.extrude(y4, x4, y3, x3)
-
-		# circle of "Q", approximated in 200 sectors
-		Pi = 3.14159265358979323846
-		NumSectors = 200
-
-		for i in range(NumSectors):
-			angle1 = (i * 2 * Pi) / NumSectors
-			x5 = 0.30 * math.sin(angle1)
-			y5 = 0.30 * math.cos(angle1)
-			x6 = 0.20 * math.sin(angle1)
-			y6 = 0.20 * math.cos(angle1)
-
-			angle2 = ((i + 1) * 2 * Pi) / NumSectors
-			x7 = 0.20 * math.sin(angle2)
-			y7 = 0.20 * math.cos(angle2)
-			x8 = 0.30 * math.sin(angle2)
-			y8 = 0.30 * math.cos(angle2)
-
-			self.quad(x5, y5, x6, y6, x7, y7, x8, y8)
-
-			self.extrude(x6, y6, x7, y7)
-			self.extrude(x8, y8, x5, y5)
-
-		GL.glEnd()
-		GL.glEndList()
-
-		return genList
-
-	def quad(self, x1, y1, x2, y2, x3, y3, x4, y4):
-		self.qglColor(self.trolltechGreen)
-
-		GL.glVertex3d(x1, y1, -0.05)
-		GL.glVertex3d(x2, y2, -0.05)
-		GL.glVertex3d(x3, y3, -0.05)
-		GL.glVertex3d(x4, y4, -0.05)
-
-		GL.glVertex3d(x4, y4, +0.05)
-		GL.glVertex3d(x3, y3, +0.05)
-		GL.glVertex3d(x2, y2, +0.05)
-		GL.glVertex3d(x1, y1, +0.05)
-
-	def extrude(self, x1, y1, x2, y2):
-		self.qglColor(self.trolltechGreen.darker(250 + int(100 * x1)))
-
-		GL.glVertex3d(x1, y1, +0.05)
-		GL.glVertex3d(x2, y2, +0.05)
-		GL.glVertex3d(x2, y2, -0.05)
-		GL.glVertex3d(x1, y1, -0.05)
 
 	def normalizeAngle(self, angle):
 		while angle < 0:
